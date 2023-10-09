74912e2b0e5640ddda788e373af0c08aaf0ee9b1
klendathu2k <jwebb@bnl.gov>
2023-10-09 12:20:50 -0400

    [Production 9 October 2023] Script to submit 2023 calorimeter production

---
+++ RHIC2023/CALOR/scripts/runall.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash -f
+
+input="ProdFlow/RHIC2023/CALOR/scripts/runnumbers"
+#input="failed_runs_short.txt"
+while IFS= read -r line
+do
+  echo "$line"
+  #runno="000"$line
+  #echo $runno
+  source ProdFlow/RHIC2023/CALOR/scripts/run_calorimeter.sh $line 0
+done < "$input"
+
