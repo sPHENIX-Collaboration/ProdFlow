66dfae381ec101d50000359e45abb0e44c7d4b83
klendathu2k <jwebb@bnl.gov>
2025-03-18 16:05:47 -0400

    Cups cleans up during stageout.

--- run3auau/TrackingProduction/stageout.sh
+++ run3auau/TrackingProduction/stageout.sh
@@ -52,9 +52,6 @@ nevents=${nevents_:--1}
 echo ./cups.py -r ${runnumber} -s ${segment} -d ${dstname}  stageout ${filename} ${destination} --dsttype ${dsttype} --dataset ${build}_${dbtag} --nevents ${nevents} --inc --prodtype only
      ./cups.py -r ${runnumber} -s ${segment} -d ${dstname}  stageout ${filename} ${destination} --dsttype ${dsttype} --dataset ${build}_${dbtag} --nevents ${nevents} --inc --prodtype only
 
-# cleanup the file once it is staged so the condor's copy back does not get it
-rm ${filename}
-
 
 echo stageout ${filename} ${destination} finish `date`
 
