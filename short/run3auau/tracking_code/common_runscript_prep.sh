09583d21e7159273ff25fde3d784bebb52b01d76
Kolja Kauder <kkauder@gmail.com>
2025-07-24 16:21:29 -0400

    Unifiying startup script

--- short/run3auau/tracking_code/common_runscript_prep.sh
+++ short/run3auau/tracking_code/common_runscript_prep.sh
@@ -2,21 +2,20 @@
 
 ## Logging details
 echo Hostname: `hostname`
-echo This setup script: $0
 echo Working directory: $_CONDOR_SCRATCH_DIR
 echo
-return
-MIN_ARG_COUNT=17
-MAX_ARG_COUNT=18
-if [ "$#" -lt "$MIN_ARG_COUNT" ] || [ "$#" -gt "$MAX_ARG_COUNT" ] ; then
+
+# MIN_ARG_COUNT=17
+# MAX_ARG_COUNT=18
+# if [ "$#" -lt "$MIN_ARG_COUNT" ] || [ "$#" -gt "$MAX_ARG_COUNT" ] ; then
+#     echo "Error: Incorrect number of arguments."
+#     echo "Expected $MIN_ARG_COUNT--$MAX_ARG_COUNT, but received $#."
+
+ARG_COUNT=16
+if [ "$#" -lt $ARG_COUNT ]  ; then
     echo "Error: Incorrect number of arguments."
-    echo "Expected $MIN_ARG_COUNT--$MAX_ARG_COUNT, but received $#."
-    echo "Arguments received: $@"
-    # argv=("$@")
-    # for i in `seq 1 16` ; do 
-    # 	echo $i ${argv[i]}
-    # done	
-    echo "Usage: $0 <nevents> <outbase> <logbase> <dsttype> <run> <seg> <outdir> <finaldir> <buildarg> <dbtag> <inputs> <dataset> <neventsper> <log_dir> <comment_str> <hist_dir> <condor_rsync_val> [dbid]"
+    echo "Expected $ARG_COUNT [+1], but received $#."
+    echo "Usage: $0 <nevents> <outbase> <logbase> <dsttype> <run> <seg> <daqhost> <outdir> <buildarg> <dbtag> <input> <dataset> <neventsper> <log_dir> <hist_dir> <condor_rsync_val> [dbid]"
     exit 1
 fi
 
@@ -25,54 +24,50 @@ nevents="$1"; shift
 outbase="$1"; shift
 logbase="$1"; shift
 dsttype="$1"; shift
-runnumber="$1"; shift
-segment="$1"; shift
+run="$1"; shift
+seg="$1"; shift
+daqhost="$1"; shift
 outdir="$1"; shift
-finaldir="$1"; shift
 buildarg="$1"; shift
 dbtag="$1"; shift
+input="$1"; shift
 dataset="$1"; shift
-ranges="$1"; shift
 neventsper="$1"; shift
 logdir="$1"; shift
-comment="$1"; shift
 histdir="$1"; shift
-
 condor_rsync="$1"; shift       # Corresponds to {rsync}
-condor_rsync=`echo $condor_rsync|sed 's/,/ /g'` # Change from comma separation
-
 dbid=${1:--1};shift            # dbid for faster db lookup, -1 means no dbid
+
+condor_rsync=`echo $condor_rsync|sed 's/,/ /g'` # Change from comma separation
 export PRODDB_DBID=$dbid
 
-
-
-echo dst should be $dsttype
-(return 0 2>/dev/null) && ( echo "Script is being sourced." ) || (echo Slipped past) && exit 0
-echo fooled ya.
-exit
-
-# Variables for the script
+# Variables for all run scripts
 echo "Processing job with the following parameters:"
 echo "---------------------------------------------"
 echo "Number of events to process (nevents): $nevents"
 echo "Output base name (outbase):            $outbase"
 echo "Log base name (logbase):               $logbase"
-echo "Input name maske (dsttype):            $dsttype"
-echo "Run number (run):                      $runnumber"
-echo "Segment number (seg):                  $segment"
+echo "Job type ~ output name stub (dsttype): $dsttype"
+echo "Run number (run):                      $run"
+echo "Segment number (seg):                  $seg"
+echo "DAQ host, leaf (daqhost)               $daqhost"
 echo "Output directory (outdir):             $outdir"
-echo "Final destination (finaldir):          $finaldir   (not used)"
 echo "Build argument (buildarg):             $buildarg"
-echo "Tag (dbtag):                           $dbtag"
-echo "Ranges string (ranges):                $ranges_string"
+echo "Tag (\"triplet\") (dbtag):               $dbtag"
+echo "Input mode (input):                    $input (all segments or seg0 only, or list in some cases)"
+echo "Dataset:                               $dataset"
 echo "Events per sub-job (neventsper):       $neventsper"
 echo "Log directory (logdir):                $logdir"
-echo "Job comment (comment):                 $comment"
 echo "Histogram directory (histdir):         $histdir"
-echo "Condor Rsync Paths (rsync):            $condor_rsync" 
 echo "Job database id (dbid):                $dbid"
 echo "---------------------------------------------"
+echo "Files/directrories to stage in (condor_rsync):"
+echo "$condor_rsync" 
+echo "---------------------------------------------"
 
+(return 0 2>/dev/null) && ( echo "Leaving sourced script." ) || (echo Exiting) && exit 0
+
+exit
 ## Make sure logfiles are kept even when receiving a signal
 sighandler()
 {
