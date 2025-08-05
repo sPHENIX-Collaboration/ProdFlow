f47f73ce600e789da982c13be0ac04838ef46178
Kolja Kauder <kkauder@gmail.com>
2025-08-05 14:50:05 -0400

    Change to common init script; unified naming scheme

--- short/run3auau/streaming_code/run_parallel_streams.sh
+++ short/run3auau/streaming_code/run_parallel_streams.sh
@@ -1,133 +1,18 @@
 #!/usr/bin/env bash
 
-## Logging details
-echo Hostname: `hostname`
-echo This script: $0
-#echo Arguments: $@
-echo Working directory: $_CONDOR_SCRATCH_DIR
-echo
-
-MIN_ARG_COUNT=17
-MAX_ARG_COUNT=18
-if [ "$#" -lt "$MIN_ARG_COUNT" ] || [ "$#" -gt "$MAX_ARG_COUNT" ] ; then
-    echo "Error: Incorrect number of arguments."
-    echo "Expected $MIN_ARG_COUNT--$MAX_ARG_COUNT, but received $#."
-    echo "Arguments received: $@"
-    echo "Usage: $0 <nevents> <outbase> <logbase> <run> <seg> <daqhost> <outdir> <finaldir> <buildarg> <tag> <inputs> <ranges_str> <neventsper> <log_dir> <comment_str> <hist_dir> <condor_rsync_val>"
-    exit 1
-fi
-
-# Parse arguments using shift
-nevents="$1"; shift
-outbase="$1"; shift
-logbase="$1"; shift
-runnumber="$1"; shift
-segment="$1"; shift            # Corresponds to {seg}
-daqhost="$1"; shift            # Corresponds to {daqhost}
-outdir="$1"; shift
-finaldir="$1"; shift
-build_argument="$1"; shift     # Corresponds to {buildarg}
-dbtag="$1"; shift
-inputs="$1"; shift
-ranges_string="$1"; shift      # Corresponds to $(ranges)
-neventsper="$1"; shift
-logdir="$1"; shift
-comment="$1"; shift
-histdir="$1"; shift # Corresponds to {histdir}
-
-condor_rsync="$1"; shift       # Corresponds to {rsync}
-condor_rsync=`echo $condor_rsync|sed 's/,/ /g'` # Change from comma separation
-
-dbid=${1:--1};shift            # dbid for faster db lookup, -1 means no dbid
-export PRODDB_DBID=$dbid
-
-# Variables for the script
-echo "Processing job with the following parameters:"
-echo "---------------------------------------------"
-echo "Number of events to process (nevents): $nevents"
-echo "Output base name (outbase):            $outbase"
-echo "Log base name (logbase):               $logbase"
-echo "Run number (run):                      $runnumber"
-echo "Segment number (seg):                  $segment           (not used)"
-echo "DAQ host (daqhost):                    $daqhost"
-echo "Output directory (outdir):             $outdir"
-echo "Final destination (finaldir):          $finaldir   (not used)"
-echo "Build argument (buildarg):             $build_argument"
-echo "Tag (tag):                             $dbtag"
-echo "Ranges string (ranges):                $ranges_string"
-echo "Events per sub-job (neventsper):       $neventsper"
-echo "Log directory (logdir):                $logdir"
-echo "Job comment (comment):                 $comment"
-echo "Histogram directory (histdir):         $histdir"
-echo "Condor Rsync Paths (rsync):            $condor_rsync" 
-echo "Job database id (dbid):                $dbid"
-echo "---------------------------------------------"
-echo "Input mode (inputs):                   $inputs (all segments or seg 0 only)"
-echo "---------------------------------------------"
-
-## Make sure logfiles are kept even when receiving a signal
-sighandler()
-{
-mv ${logbase}.out ${logdir#file:/}
-mv ${logbase}.err ${logdir#file:/}
-}
-trap sighandler SIGTERM 
-trap sighandler SIGSTOP 
-trap sighandler SIGINT 
-# SIGKILL can't be trapped
-# Note: Original jobwrapper.sh has more intricate trapping that
-# catches more signals and updates the prod db.
-
-# stage in the payload files
-cd $_CONDOR_SCRATCH_DIR
-echo Copying payload data to `pwd`
-for f in ${condor_rsync}; do
-    cp --verbose -r $f . 
-done
-ls -ltra
-echo "---------------------------------------------"
-
-export USER="$(id -u -n)"
-export LOGNAME=${USER}
-export HOME=/sphenix/u/${USER}
-
-OS=$( grep ^PRETTY_NAME /etc/os-release | sed 's/"//g'| cut -f2- -d'=' ) # Works better, though still mostly for RHEL
-if [[ $OS == "" ]]; then
-    echo "Unable to determine OS version."
-else
-    # Set up environment
-    if [[ "$_CONDOR_JOB_IWD" =~ "/Users/eickolja" ]]; then
-        source /Users/eickolja/sphenix/sphenixprod/mac_this_sphenixprod.sh
-    elif [[ $OS =~ "AlmaLinux" ]]; then
-        echo "Setting up Production software for ${OS}"
-        source /opt/sphenix/core/bin/sphenix_setup.sh -n $build_argument
-    else
-	echo "Unsupported OS $OS"
-	return 1
-    fi
-fi
-printenv
-
-echo "Offline main "${OFFLINE_MAIN}
-
-if [ -e odbc.ini ]; then
-echo export ODBCINI=./odbc.ini
-     export ODBCINI=./odbc.ini
-else
-     echo No odbc.ini file detected.  Using system odbc.ini
-fi
+## Tedium common to all run scripts. Important, source, not execute!
+echo Sourcing ${SPHENIXPROD_SCRIPT_PATH}/common_runscript_prep.sh
+. ${SPHENIXPROD_SCRIPT_PATH}/common_runscript_prep.sh
+echo "Initialization done; back in $0"
+##
 
 echo "---------------------------------------------"
-echo "Running streaming eventcombine for run ${run_number} on ${daqhost}"
+echo "Running streaming eventcombine for run ${run} on ${daqhost}"
 echo "---------------------------------------------"
 echo "--- Collecting input files"
-./create_filelist_run_daqhost.py $runnumber $daqhost $inputs
-ls -la *.list
-echo end of ls -la '*.list'
-for l in *list; do
-    echo cat $l
-    cat $l
-done
+make_filelists="./create_filelist_run_daqhost.py $run $daqhost $inputs"
+echo "$make_filelists"
+eval "$make_filelists"
 
 # Should be exactly one gl1 file and one ebdc, mvtx, or intt file
 # trying to be flexible here, but we have to assume daqhost will always be lowercase and in this family
@@ -163,12 +48,20 @@ fi
 [[ $1 == *ebdc39* ]] && ebdcfile="" && tpotfile=$1
 shopt -u nullglob
 
-# # Flag job as running in production status
-# ./cups.py -r ${runnumber} -s ${segment} -d ${outbase} running
+ls -la *.list
+echo end of ls -la '*.list'
+for l in *list; do
+    echo cat $l
+    cat $l
+done
 
 echo "--- Executing macro"
-echo root.exe -q -b Fun4All_SingleStream_Combiner.C\(${nevents},${runnumber},\"${outdir}\",\"${histdir}\",\"${outbase}\",${neventsper},\"${dbtag}\",\"${gl1file}\",\"${ebdcfile}\",\"${inttfile}\",\"${mvtxfile}\",\"${tpotfile}\"\);
-root.exe -q -b Fun4All_SingleStream_Combiner.C\(${nevents},${runnumber},\"${outdir}\",\"${histdir}\",\"${outbase}\",${neventsper},\"${dbtag}\",\"${gl1file}\",\"${ebdcfile}\",\"${inttfile}\",\"${mvtxfile}\",\"${tpotfile}\"\);
+
+root_line="Fun4All_SingleStream_Combiner.C(${nevents},${run},\"${outdir}\",\"${histdir}\",\"${outbase}\",${neventsper},\"${dbtag}\",\"${gl1file}\",\"${ebdcfile}\",\"${inttfile}\",\"${mvtxfile}\",\"${tpotfile}\");"
+full_command="root.exe -q -b '${root_line}'"
+
+echo "${full_command}"
+eval "${full_command}" ;  status_f4a=$?
 
 shopt -s nullglob
 for hfile in HIST_*.root; do
@@ -182,21 +75,8 @@ done
 
 shopt -u nullglob
 
-# # Signal that the job is done
-# destname=${outdir}/${logbase}.finished
-# # change the destination filename the same way root files are treated for easy parsing
-# destname="${destname}:nevents:0"
-# destname="${destname}:first:-1"
-# destname="${destname}:last:-1"
-# destname="${destname}:md5:none"
-# destname="${destname}:dbid:${dbid}"
-# echo touch $destname
-# touch $destname
-
 # There should be no output files hanging around  (TODO add number of root files to exit code)
 ls -la 
 
-echo "script done"
-echo "---------------------------------------------"
-
-exit 0
+echo "All done"
+exit ${status_f4a:-1}
--- short/run3auau/tracking_code/run_singlejob0.sh
+++ short/run3auau/tracking_code/run_singlejob0.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
 ## Tedium common to all run scripts. Important, source, not execute!
-echo hello
-. ./common_runscript_prep.sh
+echo Sourcing ${SPHENIXPROD_SCRIPT_PATH}/common_runscript_prep.sh
+. ${SPHENIXPROD_SCRIPT_PATH}/common_runscript_prep.sh
 echo "Initialization done; back in $0"
-echo "Running clustering (job0) for run ${run_number}, seg {segment}"
+##
+
+echo "Running clustering (job0) for run ${run}, seg ${seg}"
 echo "---------------------------------------------"
 echo "--- Collecting input files"
 echo dataset=$dataset
 echo dsttype=$dsttype
-echo dbtag=$dbtag
+echo intriplet=$intriplet
 echo run=$run
-echo seg=$se
+echo seg=$seg
 echo "---------------------------------------------"
 
-make_filelists="./create_full_filelist_run_seg.py $dataset $dbtag $dsttype $run $seg"
+make_filelists="./create_full_filelist_run_seg.py $dataset $intriplet $dsttype $run $seg"
 echo "$make_filelists"
 eval "$make_filelists"
 
@@ -22,7 +24,7 @@ shopt -s nullglob
 listsfound="$(echo *.list)"
 shopt -u nullglob
 if [[ -n $listsfound ]]; then
-    echo "Found json file(s):" 
+    echo "Found list file(s):" 
     ls -la *.list
     for l in *list; do
 	echo ---
@@ -32,12 +34,12 @@ if [[ -n $listsfound ]]; then
     echo ---
 fi
 
-run_macro='root.exe -q -b Fun4All_SingleJob0.C\(${nevents},${runnumber},\"${logbase}.root\",\"${dbtag}\",\"infile.list\"\);'
-echo $run_macro
-eval $run_macro
-exit
-# echo root.exe -q -b Fun4All_SingleJob0.C\(${nevents},${runnumber},\"${logbase}.root\",\"${dbtag}\",\"infile.list\"\)
-# root.exe -q -b Fun4All_SingleJob0.C\(${nevents},${runnumber},\"${logbase}.root\",\"${dbtag}\",\"infile.list\"\);  status_f4a=$?
+root_line="Fun4All_SingleJob0.C(${nevents},${run},\"${logbase}.root\",\"${dbtag}\",\"infile.list\")"
+full_command="root.exe -q -b '${root_line}'"
+
+echo "--- Executing macro"
+echo "${full_command}"
+eval "${full_command}" ;  status_f4a=$?
 
 ls -la
 
@@ -51,17 +53,6 @@ done
 
 ls -la
 
-echo done
+echo All done
 exit ${status_f4a:-1}
 
-
-# # Flag run as finished. 
-# echo ./cups.py -v -r ${runnumber} -s ${segment} -d ${outbase} finished -e ${status_f4a} --nevents ${nevents}  
-#      ./cups.py -v -r ${runnumber} -s ${segment} -d ${outbase} finished -e ${status_f4a} --nevents ${nevents}
-
-# echo "bdee bdee bdee, That's All Folks!"
-
-# } >> ${logdir#file:/}/${logbase}.out  2>${logdir#file:/}/${logbase}.err
-
-
-# exit ${status_f4a:-1}
