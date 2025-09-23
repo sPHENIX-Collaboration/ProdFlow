44c0ad56d174e4e4c9d77b45ae52fee7bce3f1c0
Kolja Kauder <kkauder@gmail.com>
2025-09-23 13:56:31 -0400

    Add CALOFITTING

--- short/run3auau/calo_code/Fun4All_Year2_Fitting.C
+++ short/run3auau/calo_code/Fun4All_Year2_Fitting.C
@@ -1,8 +1,12 @@
 #ifndef FUN4ALL_YEAR2_FITTING_C
 #define FUN4ALL_YEAR2_FITTING_C
 
-#include <QA.C>
 #include <Calo_Fitting.C>
+#include <QA.C>
+
+#include <calotrigger/TriggerRunInfoReco.h>
+
+#include <calovalid/CaloFittingQA.h>
 
 #include <ffamodules/CDBInterface.h>
 #include <ffamodules/FlagHandler.h>
@@ -16,32 +20,33 @@
 #include <fun4all/Fun4AllServer.h>
 #include <fun4all/Fun4AllUtils.h>
 #include <fun4all/SubsysReco.h>
-#include <calotrigger/TriggerRunInfoReco.h>
 
 #include <phool/recoConsts.h>
 
-#include <calovalid/CaloFittingQA.h>
+#include <calopacketskimmer/CaloPacketSkimmer.h>
 
-R__LOAD_LIBRARY(libfun4all.so)
 R__LOAD_LIBRARY(libfun4allraw.so)
 R__LOAD_LIBRARY(libcalovalid.so)
 R__LOAD_LIBRARY(libcalotrigger.so)
-  
+R__LOAD_LIBRARY(libCaloPacketSkimmer.so)
+
 // this pass containis the reco process that's stable wrt time stamps(raw tower building)
 void Fun4All_Year2_Fitting(int nEvents = 100,
-			   const std::string &fname = "DST_TRIGGERED_EVENT_run2pp_new_2024p003-00048185-0000.root",
-                   const std::string &outfile = "DST_CALOFITTING-00000000-000000.root",
-                   const std::string& outfile_hist= "HIST_CALOFITTINGQA-00000000-000000.root",
-                   const std::string &dbtag = "ProdA_2024")
+			   const std::string inlist = "files.list",
+                           const std::string &outfile = "DST_CALOFITTING_run3auau_new_newcdbtag_v008-00054530-00000.root",
+                           const std::string &outfile_hist = "HIST_CALOFITTINGQA_run3auau_new_newcdbtag_v008-00054530-00000.root",
+                           const std::string &dbtag = "newcdbtag")
 {
+  gSystem->Load("libg4dst.so");
+
   Fun4AllServer *se = Fun4AllServer::instance();
-  se->Verbosity(0);
+  se->Verbosity(1);
+  se->VerbosityDownscale(10000);
 
   recoConsts *rc = recoConsts::instance();
 
-  pair<int, int> runseg = Fun4AllUtils::GetRunSegment(fname);
+  pair<int, int> runseg = Fun4AllUtils::GetRunSegment(outfile);
   int runnumber = runseg.first;
-
   // conditions DB flags and timestamp
   rc->set_StringFlag("CDB_GLOBALTAG", dbtag);
   rc->set_uint64Flag("TIMESTAMP", runnumber);
@@ -54,25 +59,56 @@ void Fun4All_Year2_Fitting(int nEvents = 100,
   TriggerRunInfoReco *triggerinfo = new TriggerRunInfoReco();
   se->registerSubsystem(triggerinfo);
 
+  CaloPacketSkimmer *calopacket = new CaloPacketSkimmer();
+  se->registerSubsystem(calopacket);
+
   Process_Calo_Fitting();
 
   ///////////////////////////////////
-  // Validation 
+  // Validation
   CaloFittingQA *ca = new CaloFittingQA("CaloFittingQA");
-  //ca->set_debug(false);
   se->registerSubsystem(ca);
 
-  Fun4AllInputManager *In = new Fun4AllDstInputManager("in");
-  In->AddFile(fname);
-  se->registerInputManager(In);
-
-  Fun4AllDstOutputManager *out = new Fun4AllDstOutputManager("DSTOUT", outfile);
-  out->StripNode("CEMCPackets");
-  out->StripNode("HCALPackets");
-  out->StripNode("ZDCPackets");
-  out->StripNode("SEPDPackets");
-  se->registerOutputManager(out);
-
+  // Fun4AllInputManager *In = new Fun4AllDstInputManager("in");
+  // In->AddFile(fname);
+  // se->registerInputManager(In);
+  Fun4AllInputManager *In = nullptr;
+  ifstream infile;
+  infile.open(inlist);
+    int iman = 0;
+    std::string line;
+    if (infile.is_open())
+    {
+      while (std::getline(infile, line))
+      {
+	if (line[0] == '#')
+	{
+	  std::cout << "found commented out line " << line << std::endl;
+	  continue;
+	}
+	std::cout << line << std::endl;
+	std::string magname = "DSTin_" + std::to_string(iman);
+	In = new Fun4AllDstInputManager(magname);
+	In->Verbosity(1);
+	In->AddFile(line);
+	se->registerInputManager(In);
+	iman++;
+      }
+      infile.close();
+    }
+    if (iman == 0)
+    {
+      std::cout << "No files in filelist" << std::endl;
+      gSystem->Exit(1);
+    }
+       Fun4AllDstOutputManager *out = new Fun4AllDstOutputManager("DSTOUT", outfile);
+   out->StripCompositeNode("Packets");
+   se->registerOutputManager(out);
+  // se->Print();
+  if (nEvents < 0)
+  {
+    return;
+  }
   se->run(nEvents);
   se->End();
 
--- short/run3auau/calo_code/run_fitting.sh
+++ short/run3auau/calo_code/run_fitting.sh
@@ -1,166 +1,76 @@
 #!/usr/bin/env bash
 
-## Logging details
-echo Hostname: `hostname`
-echo This script: $0
-echo Working directory: $_CONDOR_SCRATCH_DIR
-echo
+## Tedium common to all run scripts. Important, source, not execute!
+echo Sourcing ${SPHENIXPROD_SCRIPT_PATH}/common_runscript_prep.sh
+. ${SPHENIXPROD_SCRIPT_PATH}/common_runscript_prep.sh
+echo "Initialization done; back in $0"
+##
 
-MIN_ARG_COUNT=17
-MAX_ARG_COUNT=18
-if [ "$#" -lt "$MIN_ARG_COUNT" ] || [ "$#" -gt "$MAX_ARG_COUNT" ] ; then
-    echo "Error: Incorrect number of arguments."
-    echo "Expected $MIN_ARG_COUNT--$MAX_ARG_COUNT, but received $#."
-    echo "Arguments received: $@"
-    echo "Usage: $0 <nevents> <outbase> <logbase> <inbase> <run> <seg> <outdir> <finaldir> <buildarg> <tag> <inputs> <ranges_str> <neventsper> <log_dir> <comment_str> <hist_dir> <condor_rsync_val>"
-    exit 1
-fi
-
-
-# Parse arguments using shift
-nevents="$1"; shift
-outbase="$1"; shift
-logbase="$1"; shift
-inbase="$1"; shift
-runnumber="$1"; shift
-segment="$1"; shift
-outdir="$1"; shift
-finaldir="$1"; shift
-buildarg="$1"; shift
-dbtag="$1"; shift
-inputs="$1"; shift
-ranges="$1"; shift
-neventsper="$1"; shift
-logdir="$1"; shift
-comment="$1"; shift
-histdir="$1"; shift
-
-condor_rsync="$1"; shift       # Corresponds to {rsync}
-condor_rsync=`echo $condor_rsync|sed 's/,/ /g'` # Change from comma separation
-
-dbid=${1:--1};shift            # dbid for faster db lookup, -1 means no dbid
-export PRODDB_DBID=$dbid
-
-
-# Variables for the script
-echo "Processing job with the following parameters:"
-echo "---------------------------------------------"
-echo "Number of events to process (nevents): $nevents"
-echo "Output base name (outbase):            $outbase"
-echo "Log base name (logbase):               $logbase"
-echo "Input name maske (inbase):             $inbase"
-echo "Run number (run):                      $runnumber"
-echo "Segment number (seg):                  $segment"
-echo "Output directory (outdir):             $outdir"
-echo "Final destination (finaldir):          $finaldir   (not used)"
-echo "Build argument (buildarg):             $buildarg"
-echo "Tag (tag):                             $dbtag"
-echo "Ranges string (ranges):                $ranges_string"
-echo "Events per sub-job (neventsper):       $neventsper"
-echo "Log directory (logdir):                $logdir"
-echo "Job comment (comment):                 $comment"
-echo "Histogram directory (histdir):         $histdir"
-echo "Condor Rsync Paths (rsync):            $condor_rsync" 
-echo "Job database id (dbid):                $dbid"
-echo "---------------------------------------------"
-
-## Make sure logfiles are kept even when receiving a signal
-sighandler()
-{
-mv ${logbase}.out ${logdir#file:/}
-mv ${logbase}.err ${logdir#file:/}
-}
-trap sighandler SIGTERM 
-trap sighandler SIGSTOP 
-trap sighandler SIGINT 
-# SIGKILL can't be trapped
-
-# stage in the payload files
-cd $_CONDOR_SCRATCH_DIR
-echo Copying payload data to `pwd`
-for f in ${condor_rsync}; do
-    cp --verbose -r $f . 
-done
-ls -ltra
-echo "---------------------------------------------"
-
-
-
-export USER="$(id -u -n)"
-export LOGNAME=${USER}
-export HOME=/sphenix/u/${USER}
-
-OS=$( grep ^PRETTY_NAME /etc/os-release | sed 's/"//g'| cut -f2- -d'=' ) # Works better, though still mostly for RHEL
-if [[ $OS == "" ]]; then
-    echo "Unable to determine OS version."
-else
-    # Set up environment
-    if [[ "$_CONDOR_JOB_IWD" =~ "/Users/eickolja" ]]; then
-        source /Users/eickolja/sphenix/sphenixprod/mac_this_sphenixprod.sh
-    elif [[ $OS =~ "AlmaLinux" ]]; then
-        echo "Setting up Production software for ${OS}"
-        source /opt/sphenix/core/bin/sphenix_setup.sh -n $buildarg
-    else
-	echo "Unsupported OS $OS"
-	return 1
-    fi
-fi
-# printenv
-
-echo "Offline main "${OFFLINE_MAIN}
-
-if [ -e odbc.ini ]; then
-echo export ODBCINI=./odbc.ini
-     export ODBCINI=./odbc.ini
-else
-     echo No odbc.ini file detected.  Using system odbc.ini
-fi
-
-
-echo "---------------------------------------------"
 echo "Running CALOFITTING for run ${run_number}, seg {segment}"
 echo "---------------------------------------------"
 echo "--- Collecting input files"
-echo inbase=$inbase
-echo runnumber=$runnumber
-echo segment=$segment
+echo dataset=$dataset
+echo dsttype=$dsttype
+echo intriplet=$intriplet
+echo run=$run
+echo seg=$seg
+echo "---------------------------------------------"
 
-echo 'create_filelist_run_seg.py $inbase $runnumber $segment'
-./create_filelist_run_seg.py $inbase $runnumber $segment
+make_filelists="./create_full_filelist_run_seg.py $dataset $intriplet $dsttype $run $seg"
+echo "$make_filelists"
+eval "$make_filelists"
+
 ls -la *.list
 echo end of ls -la '*.list'
-for l in *list; do
-    echo cat $l
-    cat $l
+
+### Stage input to local
+for fullinfile in `cat infile_paths.list`; do
+    cp -v $fullinfile .
 done
 
-exit
+outfile=${logbase}.root
+outhist=${outfile/DST_CALOFITTING/HIST_CALOFITTINGQA}
+root_line="Fun4All_Year2_Fitting.C(${nevents},\"infile.list\",\"${outfile}\",\"${outhist}\",\"${dbtag}\")"
+full_command="root.exe -q -b '${root_line}'"
+eval "${full_command}"
+
+for hfile in HIST_*.root; do
+    echo stageout.sh ${hfile} to ${histdir}
+    ./stageout.sh ${hfile} ${histdir}
+done
+
+echo ./stageout.sh ${logbase}.root ${outdir} ${dbid}
+./stageout.sh ${logbase}.root ${outdir} ${dbid}
 
 
 
-nevents=-1
-status_f4a=0
+ls -la
 
-for infile_ in ${inputs[@]}; do
-    outfile=${logbase}.root
-    outhist=${outfile/DST_CALOFITTING/HIST_CALOFITTINGQA}
-./cups.py -r ${runnumber} -s ${segment} -d ${outbase} running
+echo done
+exit ${status_f4a:-1}
+
+# status_f4a=0
+
+# for infile_ in ${inputs[@]}; do
+#     outfile=${logbase}.root
+#     outhist=${outfile/DST_CALOFITTING/HIST_CALOFITTINGQA}
+# ./cups.py -r ${runnumber} -s ${segment} -d ${outbase} running
     
-    root.exe -q -b Fun4All_Year2_Fitting.C\(${nevents},\"${infile}\",\"${outfile}\",\"${outhist}\",\"${dbtag}\"\);  status_f4a=$?
+#     root.exe -q -b Fun4All_Year2_Fitting.C\(${nevents},\"${infile}\",\"${outfile}\",\"${outhist}\",\"${dbtag}\"\);  status_f4a=$?
 
-    nevents=${nevents_:--1}
-    echo Stageout ${outfile} to ${outdir}
-        ./stageout.sh ${outfile} ${outdir}
+#     nevents=${nevents_:--1}
+#     echo Stageout ${outfile} to ${outdir}
+#         ./stageout.sh ${outfile} ${outdir}
  
-    for hfile in `ls HIST_*.root`; do
-	echo Stageout ${hfile} to ${histdir}
-        ./stageout.sh ${hfile} ${histdir}
-    done
+#     for hfile in `ls HIST_*.root`; do
+# 	echo Stageout ${hfile} to ${histdir}
+#         ./stageout.sh ${hfile} ${histdir}
+#     done
 
-done
+# done
     
 
-exit ${status_f4a}
+# exit ${status_f4a}
 
 
 
