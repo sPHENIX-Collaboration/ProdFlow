f2962e49efd46d8c269b960eb20ca2cd54f294ed
Kolja Kauder <kkauder@gmail.com>
2026-03-02 16:44:48 -0500

    Repoloacing outdated calo code

--- run3oo_calo_prod1/calo_code/Fun4All_Prdf_Combiner.C
+++ run3oo_calo_prod1/calo_code/Fun4All_Prdf_Combiner.C
@@ -1,10 +1,10 @@
-#include <fun4all/Fun4AllServer.h>
-#include <fun4all/Fun4AllInputManager.h>
 #include <fun4all/Fun4AllDstOutputManager.h>
-#include <fun4allraw/SingleGl1TriggeredInput.h>
+#include <fun4all/Fun4AllInputManager.h>
+#include <fun4all/Fun4AllOutputManager.h>
+#include <fun4all/Fun4AllServer.h>
 #include <fun4allraw/Fun4AllTriggeredInputManager.h>
 #include <fun4allraw/InputManagerType.h>
-#include <fun4all/Fun4AllOutputManager.h>
+#include <fun4allraw/SingleGl1TriggeredInput.h>
 
 #include <ffamodules/FlagHandler.h>
 #include <ffamodules/HeadReco.h>
@@ -12,9 +12,9 @@
 
 #include <ffarawmodules/ClockDiffCheck.h>
 
-#include <TSystemDirectory.h>
-#include <TSystemFile.h>
-#include <TString.h>
+#include <TSystem.h>
+
+#include <filesystem>
 
 R__LOAD_LIBRARY(libfun4all.so)
 R__LOAD_LIBRARY(libfun4allraw.so)
@@ -22,12 +22,10 @@ R__LOAD_LIBRARY(libffamodules.so)
 R__LOAD_LIBRARY(libffarawmodules.so)
 
 void Fun4All_Prdf_Combiner(int nEvents = 0,
-			   const std::string &daqhost = "seb15",
-			   const std::string &outbase = "delme",
-			   const std::string &outdir = "/sphenix/data/data02/sphnxpro/scratch/kolja/test/"
-  )
+                           const std::string &daqhost = "seb15",
+                           const std::string &outbase = "delme",
+                           const std::string &outdir = "/sphenix/data/data02/sphnxpro/scratch/kolja/test")
 {
-
   Fun4AllServer *se = Fun4AllServer::instance();
   se->Verbosity(1);
   se->VerbosityDownscale(100000);
@@ -38,58 +36,27 @@ void Fun4All_Prdf_Combiner(int nEvents = 0,
   //  gl1->Verbosity(10);
   in->registerGl1TriggeredInput(gl1);
 
-  SingleTriggeredInput *input = new SingleTriggeredInput(daqhost);
-  if (daqhost == "seb18") {
-    input->KeepPackets();
-  }
 
-  //  input->Verbosity(10);
-  //  input->FakeProblemEvent(10);
-  TSystemDirectory workdir("workdir",".");
-  TList *listfiles = workdir.GetListOfFiles();
-  TIter listnext(listfiles);
-  while ( auto listfile = (TSystemFile*) listnext() ){
-      TString fname = listfile->GetName();
-      if ( !fname.EndsWith(".list") ) continue;
-      if ( fname == "gl1daq.list") continue;
-      if ( fname.Contains( daqhost ) ){
-	ifstream infile;
-	infile.open(fname.Data());
-	std::cout << "Adding " << fname << std::endl;
-	if (infile.is_open()){
-	  infile.close();
-	  input->AddListFile(fname.Data());
-	  in->registerTriggeredInput(input);
-	  break; // don't need to break; but we should probably sort if we use multiple input lists
-	}
+  for (const auto &entry : std::filesystem::directory_iterator("."))
+  {
+    std::string fname = entry.path().filename().string();
+    if (!fname.ends_with(".list")) continue;
+    if (fname == "gl1daq.list") continue;
+    if (fname.find(daqhost) != std::string::npos)
+    {
+      std::ifstream infile;
+      infile.open(fname);
+      std::cout << "Adding " << fname << std::endl;
+      if (infile.is_open())
+      {
+        infile.close();
+	SingleTriggeredInput *input = new SingleTriggeredInput(daqhost);
+        input->AddListFile(fname);
+        in->registerTriggeredInput(input);
       }
     }
+  }
   se->registerInputManager(in);
-  // In principle, more than one input file list can be supported
-  // std::vector<TString> filenames; // collect names first so they can be ordered
-  // std::sort(filenames.begin(), filenames.end());
-
-  
-//   ifstream infile;
-//   SingleTriggeredInput *input = nullptr;
-//   for (int i=0; i<21; i++)
-//   {
-//     char daqhost[200];
-//     char daqlist[200];
-//     sprintf(daqhost,"seb%02d",i);
-//     sprintf(daqlist,"%s.list",daqhost);
-//     infile.open(daqlist);
-//     if (infile.is_open())
-//     {
-//       infile.close();
-//       input = new SingleTriggeredInput(daqhost);
-// //  input->Verbosity(10);
-// //      input->FakeProblemEvent(10);
-//       input->AddListFile(daqlist);
-//       in->registerTriggeredInput(input);
-//     }
-//   }
-//   se->registerInputManager(in);
 
   SyncReco *sync = new SyncReco();
   se->registerSubsystem(sync);
@@ -100,27 +67,22 @@ void Fun4All_Prdf_Combiner(int nEvents = 0,
   se->registerSubsystem(flag);
 
   ClockDiffCheck *clkchk = new ClockDiffCheck();
-//   clkchk->Verbosity(3);
+  //   clkchk->Verbosity(3);
   clkchk->set_delBadPkts(true);
   se->registerSubsystem(clkchk);
   // std::string outfile = "DST_TRIGGERED_EVENT_" + daqhost + "_run2pp_new_nocdbtag_v001.root";
   std::string outfile = outbase + ".root";
-  Fun4AllOutputManager *out = new Fun4AllDstOutputManager("dstout",outfile);
-  out->SplitLevel(0);
+  Fun4AllOutputManager *out = new Fun4AllDstOutputManager("dstout", outfile);
   out->UseFileRule();
-  out->SetNEvents(100000); 
-  // out->SetClosingScript("copyscript.pl");      // script to call on file close (not quite working yet...)
-  // out->SetClosingScriptArgs(" -mv -outdir " + outdir);  // additional beyond the name of the file
-  // out->SetClosingScript("/bin/echo");      // script to call on file close (not quite working yet...)
-  // out->SetClosingScript("/usr/bin/mv");
-  // out->SetClosingScriptArgs(" -v " + outdir);  // additional beyond the name of the file
+  out->SetEventNumberRollover(100000);
   out->SetClosingScript("./stageout.sh");
   out->SetClosingScriptArgs(outdir + " " + "0");  // additional beyond the name of the file
   se->registerOutputManager(out);
-  if (nEvents >= 0)
+  if (nEvents < 0)
   {
-    se->run(nEvents);
+    return;
   }
+  se->run(nEvents);
   se->End();
   delete se;
   std::cout << "all done" << std::endl;
